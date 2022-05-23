2571a245ccea4488eaa1fea9c534d23d321247cc
Tarek Hammami <THammami.me@gmail.com>
2022-05-23 21:59:12 +0100

    chore: add all project files

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return jsonify({'msg': 'Hello, World!'})
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    # deepcode ignore RunWithDebugTrue: <please specify a reason of ignoring this>
+    app.run(debug=True, host='0.0.0.0', port=port)
